cedeb310668a3fafa229ded298b756704ee414e2
Robin Monnier <robin.monnier.enpc@gmail.com>
2016-11-27 00:42:07 +0100

    groupy by (DATE, ASS_ASSIGNMENT) + create now hdf file for both train and submission datasets

--- preprocessing/Preprocesser.py
+++ preprocessing/Preprocesser.py
@@ -9,7 +9,11 @@ class Preprocesser:
     NB: during all preprocess pipeline, the dataframe is modified directly (not a copy of it) to prevent memory issues
     """
 
-    def drop_useless_features(self):
+    def cast_dates_into_timestamp(self):
+        # convert dates into pandas.Timestamp
+        self.df["DATE"] = self.df["DATE"].apply(lambda date: pd.Timestamp(date))
+
+    def drop_useless_columns(self):
         if "CSPL_RECEIVED_CALLS" in self.df.columns:
             self.df = self.df[["DATE", "ASS_ASSIGNMENT", "CSPL_RECEIVED_CALLS"]]
         else:
@@ -17,9 +21,6 @@ class Preprocesser:
 
     def split_dates(self):
         """ Create columns YEAR, MONTH, DAY, HOUR and MINUTE from DATE. """
-
-        # convert dates into pandas.Timestamp
-        self.df["DATE"] = self.df["DATE"].apply(lambda date: pd.Timestamp(date))
 
         # split dates
         self.df["YEAR"] = self.df["DATE"].apply(lambda date: date.year)
@@ -59,9 +60,7 @@ class Preprocesser:
 
         # holidays
 
-    def process_data(self, source_path, destination_path, sep, batch_size=100000, verbose=False):
-
-        if verbose: print("Start preprocessing...")
+    def clean_csv(self, source_path, destination_path, sep, batch_size=100000, verbose=False):
 
         # read first data
         self.df = pd.read_csv(source_path, sep=sep, nrows=batch_size)
@@ -69,24 +68,17 @@ class Preprocesser:
         # keep track of number of batchs done
         batch_nb = 0
         while True:
-
             if verbose: print("Batch #%d..." % batch_nb)
 
-            # drop columns
-            self.drop_useless_features()
-
-            # date -> year, month,...
-            self.split_dates()
-
-            # fill day-off column
-            self.fill_not_working_days()
+            # drop useless columns
+            self.drop_useless_columns()
 
             # append processed data to csv
             with open(destination_path, "w" if batch_nb == 0 else "a") as file:
                 header = (batch_nb == 0)
                 self.df.to_csv(file, index=False, header=header)
 
-            # if df has not the size of batch_size, it we just preprocessed the last batch -> we end the loop
+            # if df has not the size of batch_size, it means we just preprocessed the last batch -> we end the loop
             if batch_size != len(self.df):
                 break
 
@@ -96,4 +88,45 @@ class Preprocesser:
             # read next batch data
             self.df = pd.read_csv(source_path, sep=sep, nrows=batch_size, skiprows=np.arange(1, batch_size * batch_nb))
 
-        if verbose: print("Preprocessing done...")
+
+    def create_temporal_features(self, source_path, destination_path):
+        # read csv
+        self.df = pd.read_csv(source_path)
+
+        # cast date into pandas.Timestamp
+        self.cast_dates_into_timestamp()
+
+        # date -> year, month,...
+        self.split_dates()
+
+        # fill day-off column
+        self.fill_not_working_days()
+
+        # write csv
+        self.df.to_csv(destination_path, index=False)
+
+    def group_by_date_ass_assignment(self, source_path, destination_path):
+        # read csv
+        self.df = pd.read_csv(source_path)
+
+        # group by and sum received calls
+        group_by = self.df.groupby(["DATE", "ASS_ASSIGNMENT"])
+        self.df = pd.DataFrame(group_by["CSPL_RECEIVED_CALLS"].sum()).reset_index()
+
+        # write csv
+        self.df.to_csv(destination_path, index=False)
+
+    def csv_to_hdf(self, source_path, destination_path):
+        # read csv
+        self.df = pd.read_csv(source_path)
+
+        # retrieve list of ass_assignments
+        ass_assignments = self.df["ASS_ASSIGNMENT"].unique()
+
+        # loop over ass_assignments to store into hdf_file
+        hdf_file = pd.HDFStore(destination_path)
+        for ass_assignment in ass_assignments:
+            hdf_file[ass_assignment] = self.df[self.df["ASS_ASSIGNMENT"] == ass_assignment].drop("ASS_ASSIGNMENT", axis=1)
+
+        # close hdf file
+        hdf_file.close()
--- preprocessing_script.py
+++ preprocessing_script.py
@@ -2,18 +2,51 @@ from preprocessing import Preprocesser
 
 preprocesser = Preprocesser()
 
+# Clean csv step
+
 # train
 source_path = "data/train_2011_2012_2013.csv"
-destination_path = "data/preprocessed_train_2011_2012_2013.csv"
+destination_path = "data/clean_train.csv"
 sep = ";"
 batch_size = 500000
 
-preprocesser.process_data(source_path, destination_path, sep, batch_size=batch_size, verbose=True)
+print("Clean train csv...")
+preprocesser.clean_csv(source_path, destination_path, sep, batch_size=batch_size, verbose=True)
 
 # submission
 source_path = "data/submission.txt"
-destination_path = "data/preprocessed_submission.csv"
+destination_path = "data/clean_submission.csv"
 sep = "\t"
 batch_size = 50000
 
-preprocesser.process_data(source_path, destination_path, sep, batch_size=batch_size, verbose=True)
+print("\nClean sub csv...")
+preprocesser.clean_csv(source_path, destination_path, sep, batch_size=batch_size, verbose=True)
+
+# Group by tuple (DATE, ASS_ASSIGNMENT) for train dataset
+print("\nGroup by (DATE, ASS_ASSIGNMENT) for train dataset...")
+source_path = "data/clean_train.csv"
+destination_path = "data/grouped_by_train.csv"
+preprocesser.group_by_date_ass_assignment(source_path, destination_path)
+
+# Create temporal features
+print("\nCreate temporal features for train dataset...")
+source_path = "data/grouped_by_train.csv"
+destination_path = "data/preprocessed_train.csv"
+preprocesser.create_temporal_features(source_path, destination_path)
+
+print("\nCreate temporal features for sub dataset...")
+source_path = "data/clean_submission.csv"
+destination_path = "data/preprocessed_submission.csv"
+preprocesser.create_temporal_features(source_path, destination_path)
+
+# Csv to hdf file step
+print("\nConvert train csv file into hdf file...")
+source_path = "data/preprocessed_train.csv"
+destination_path = "data/train.h5"
+preprocesser.csv_to_hdf(source_path, destination_path)
+
+print("\nConvert sub csv file into hdf file...")
+source_path = "data/preprocessed_submission.csv"
+destination_path = "data/submission.h5"
+preprocesser.csv_to_hdf(source_path, destination_path)
+
