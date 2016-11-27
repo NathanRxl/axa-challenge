c77898cc5449c98d429c63311f92a7718fdc234a
Robin Monnier <robin.monnier.enpc@gmail.com>
2016-11-27 20:23:07 +0100

    new architecture for ML pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,31 +1,34 @@
+import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 from tools import data_loader, create_submission
 
-# parameters
-path = "data/train_2011_2012_2013.csv"
-nrows = 10000
-randomState = 42
+# object that saves prediction and gather them into a final submission file
+submissioner = Submissioner()
 
-# load data
-# TODO: create h5 file from csv with cleaned data then load data directly from h5 file
-X_train, y_train, X_test = data_loader(path, nrows)
+# loop over week of submissions
+for week_nb in np.arange(11):
+    data_loader = DataLoader(week_nb=week_nb)
 
-# classifier
-clf = Pipeline(
-    [
-        ("scaler", StandardScaler(with_mean=True, with_std=True)),
-        ("estimator", RandomForestRegressor(n_estimators=300, max_depth=15, n_jobs=-1, random_state=randomState)),
-    ]
-)
+    # loop over ass_assignments
+    for ass_assignment, dates_train, X_train, y_train, dates_test, X_test in data_loader:
 
-# fit
-clf.fit(X_train, y_train)
+        # model
+        clf = Pipeline(
+            [
+                ("scaler", StandardScaler(with_mean=True, with_std=True)),
+                ("estimator", RandomForestRegressor(n_estimators=300, max_depth=15, n_jobs=-1, random_state=42)),
+            ]
+        )
 
-# predict
-y_predict = clf.predict(X_test)
+        # fit
+        clf.fit(X_train, y_train)
 
-# create submission
-create_submission(X_test, y_predict)
+        # predict
+        y_predict = clf.predict(X_test)
+
+        submissioner.save(dates_test, ass_assignment, y_predict)
+
+submissioner.create_submission()
